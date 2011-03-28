0341a569a7d02c4603a021c72b1106efd27c9e16
Myles Grant <myles@mylesgrant.com>
2011-03-27 17:34:36 -0700

    Proper setup of the utils lib

--- Utils.h
+++ Utils.h
@@ -21,33 +21,12 @@
 
 #include "WProgram.h"
 
-// Insert sort. From "whistler" - http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1283456170/0
-void isort(int *a, byte n)
-{
-  for (int i = 1; i < n; ++i)
-  {
-    int j = a[i];
-    int k;
-    for (k = i - 1; (k >= 0) && (j < a[k]); k--)
-    {
-      a[k + 1] = a[k];
-    }
-    a[k + 1] = j;
-  }
-}
+// Convert from Gs to Meters Per Second Squared (and vice-versa)
+#define G_2_MPS2(g) (g * 9.80665)
+#define MPS2_2_G(m) (m * 0.10197162)
 
-int findMedian(int *data, byte arraySize){
-  isort(data, arraySize);
-  
-  return data[arraySize/2];
-}
-
-// Low pass filter, kept as regular C function for speed
-float filterSmooth(float currentData, float previousData, float smoothFactor) {
-  if (smoothFactor != 1.0) //only apply time compensated filter if smoothFactor is applied
-    return (previousData * (1.0 - smoothFactor) + (currentData * smoothFactor)); 
-  else
-    return currentData; //if smoothFactor == 1.0, do not calculate, just bypass!
-}
+void isort(int *, byte);
+int findMedian(int *, byte);
+float filterSmooth(float, float, float);
 
 #endif
