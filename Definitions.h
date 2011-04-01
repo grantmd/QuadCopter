f81900160544e3b77f9cbdfb924058c5b866e01c
Myles Grant <myles@mylesgrant.com>
2011-04-01 09:05:10 -0700

    Adding header/gpl to definitions file

--- Definitions.h
+++ Definitions.h
@@ -1,3 +1,24 @@
+/*
+  Global definitions for my quadcopter. Holds constants and config flags for either globally
+    useful data or things I might want to change easily. There may be other definitions elsewhere
+    in the codebase!
+  Created by Myles Grant <myles@mylesgrant.com>
+  See also: https://github.com/grantmd/QuadCopter
+  
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+  GNU General Public License for more details. 
+
+  You should have received a copy of the GNU General Public License 
+  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+*/
+
 // Config flags
 #define SENSORS_ENABLED
 #define SERIALCONTROL_ENABLED
