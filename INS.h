df177ea832d0e7de450b542a1434c32b18057c82
Myles Grant <myles@mylesgrant.com>
2011-04-14 21:10:21 -0700

    Skeleton for an INS class

---
+++ INS.h
@@ -0,0 +1,32 @@
+/*
+  INS.h - Library for implementing an Inertial Navigation System for my Quadcopter
+   i.e. a dead-reckoning system for obtaining current position based on the IMU data
+  Created by Myles Grant <myles@mylesgrant.com>
+  See also: https://github.com/grantmd/QuadCopter
+  
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+  GNU General Public License for more details. 
+
+  You should have received a copy of the GNU General Public License 
+  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+*/
+#ifndef INS_h
+#define INS_h
+
+#include "WProgram.h"
+#include "Definitions.h"
+
+class INS
+{
+  public:
+    INS();
+};
+
+#endif
